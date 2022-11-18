f0d042757be6d6cd14ea756682417eb45d28811c
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-18 18:13:36 +0100

    console.log(utente)

--- script.js
+++ script.js
@@ -1,45 +1,21 @@
 // 1. Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 
-let yuji ={
+/* let yuji = {
     name: "Yuji",
     surname: "Itadori",
     age: 15,
     occupation: "Jujutsu Sorcer"
-}
+};
 
 
 // 2. Stampare a schermo attraverso un ciclo for (o eventualmente con for-in) tutte le proprietà dell'oggetto.
 
-for (let key in yuji){
+for (let key in yuji) {
 
     console.log(key);
     console.log(yuji[key]);
-    
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}; */
 
 
 
@@ -48,43 +24,50 @@ for (let key in yuji){
 let tokyoJujutsuHigh = [
 
     {
-        name: "Yuji Itadori",
+        name: "Yuji",
+        surname: "Itadori",
         age: 15,
         occupation: "Jujutsu Sorcer"
     },
-    
+
     {
-        name: "Megumi Fushiguro",
+        name: "Megumi",
+        surname: "Fushiguro",
         age: 15,
         occupation: "Jujutsu Sorcer"
     },
-    
+
     {
-        name: "Nobara Kugisaki",
+        name: "Nobara",
+        surname: "Kugisaki",
         age: 16,
         occupation: "Jujutsu Sorcer"
     },
-    
+
     {
-        name: "Maki Zenin",
+        name: "Maki",
+        surname: "Zenin",
         age: 16,
         occupation: "Jujutsu Sorcer"
     },
-    
+
     {
-        name: "Toge Inumaki",
+        name: "Toge",
+        surname: "Inumaki",
         age: 17,
         occupation: "Jujutsu Sorcer"
     },
-    
+
     {
         name: "Panda",
+        surname: "",
         age: 17,
         occupation: "Jujutsu Sorcer"
     },
-    
+
     {
-        name: "Yuta Okkotsu",
+        name: "Yuta",
+        surname: "Okkotsu",
         age: 17,
         occupation: "Jujutsu Sorcer"
     }
@@ -95,24 +78,39 @@ let tokyoJujutsuHigh = [
 
 //4. Ciclare su tutti gli studenti e stampare per ognuno di essi: nome e cognome.
 
-printTokyoJujutsuHigh (tokyoJujutsuHigh);
+printTokyoJujutsuHigh(tokyoJujutsuHigh);
 
 //5. Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere all’array creato in precedenza, un nuovo oggetto studente inserendo nell’ ordine: nome, cognome e età.
 
-let nameUtente = prompt("Nome completo:");
+let nameUtente = prompt("Nome:");
+let surnameUtente = prompt("Cognome:");
 let ageUtente = Math.floor(prompt("Età:"));
 
-tokyoJujutsuHigh.push (
+tokyoJujutsuHigh.push(
 
-    utente ={
+    utente = {
         name: nameUtente,
+        surname: surnameUtente,
         age: ageUtente,
         occupation: "Jujutsu Sorcer"
     }
 
-)
+);
 
-console.log (utente)
+/* console.log(utente); */
+
+console.log(
+    `
+    ------------------- STUDENTE --------------------
+
+        Nome: ${utente.name}
+        Cognome: ${utente.surname}
+        Età: ${utente.age}
+        Occupazione: ${utente.occupation}
+
+    -------------------------------------------------
+    `
+);
 
 
 
@@ -120,13 +118,14 @@ console.log (utente)
 
 // -------------------- Funzioni ---------------------------
 
-function printYuji (yuji){
+function printYuji(yuji) {
 
     console.log(
         `
         ------------------- STUDENTE --------------------
 
             Nome: ${yuji[i].name}
+            Cognome: ${yuji[i].surname}
             Età: ${yuji[i].age}
             Occupazione: ${yuji[i].occupation}
 
@@ -137,35 +136,36 @@ function printYuji (yuji){
 
 // stampa array
 
-function printTokyoJujutsuHigh (tokyoJujutsuHigh) {
+function printTokyoJujutsuHigh(tokyoJujutsuHigh) {
 
-    for (let element of tokyoJujutsuHigh){
+    /* for (let element of tokyoJujutsuHigh) {
 
-        /* console.log(element); */
+        console.log(element);
 
-        for (let key in element){
+        for (let key in element) {
 
             console.log(key);
             console.log(element[key]);
-        }
-
-        
-    }
+        };
 
 
-    /* for (let i=0; i<tokyoJujutsuHigh.length; i++) {
+    }; */
+
+
+    for (let i = 0; i < tokyoJujutsuHigh.length; i++) {
 
         console.log(
             `
             ------------------- STUDENTE --------------------
 
                 Nome: ${tokyoJujutsuHigh[i].name}
+                Cognome: ${tokyoJujutsuHigh[i].surname}
                 Età: ${tokyoJujutsuHigh[i].age}
                 Occupazione: ${tokyoJujutsuHigh[i].occupation}
 
             -------------------------------------------------
             `
-        )
-    }; */
-    
-}
+        );
+    };
+
+};
