acd17bd83d3792664c909a25570f4de4c54bfdb8
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-18 19:50:35 +0100

    completato untente

--- script.js
+++ script.js
@@ -27,49 +27,55 @@ let tokyoJujutsuHigh = [
         name: "Yuji",
         surname: "Itadori",
         age: 15,
-        occupation: "Jujutsu Sorcer"
+        occupation: "Jujutsu Sorcer",
+        photo: "./img/Yuji_Itadori_29.webp"
     },
 
     {
         name: "Megumi",
         surname: "Fushiguro",
         age: 15,
-        occupation: "Jujutsu Sorcer"
+        occupation: "Jujutsu Sorcer",
+        photo: "./img/Megumi_Fushiguro_29.webp"
     },
 
     {
         name: "Nobara",
         surname: "Kugisaki",
         age: 16,
-        occupation: "Jujutsu Sorcer"
+        occupation: "Jujutsu Sorcer",
+        photo: "./img/Nobara_Kugisaki_29.webp"
     },
 
     {
         name: "Maki",
         surname: "Zenin",
         age: 16,
-        occupation: "Jujutsu Sorcer"
+        occupation: "Jujutsu Sorcer",
+        photo: "./img/Maki_Zenin_29.webp"
     },
 
     {
         name: "Toge",
         surname: "Inumaki",
         age: 17,
-        occupation: "Jujutsu Sorcer"
-    },
-
+        occupation: "Jujutsu Sorcer",
+        photo: "./img/Toge_Inumaki_29.webp"
+    },   
     {
         name: "Panda",
         surname: "",
         age: 17,
-        occupation: "Jujutsu Sorcer"
+        occupation: "Jujutsu Sorcer",
+        photo: "./img/Panda_29.webp"
     },
 
     {
         name: "Yuta",
         surname: "Okkotsu",
         age: 17,
-        occupation: "Jujutsu Sorcer"
+        occupation: "Jujutsu Sorcer",
+        photo:"./img/Yuta_Okkotsu_29.webp"
     }
 
 ];
@@ -82,20 +88,38 @@ printTokyoJujutsuHigh(tokyoJujutsuHigh);
 
 //5. Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere all’array creato in precedenza, un nuovo oggetto studente inserendo nell’ ordine: nome, cognome e età.
 
+let genere = prompt("Sei maschio o femmina?");
 let nameUtente = prompt("Nome:");
 let surnameUtente = prompt("Cognome:");
 let ageUtente = Math.floor(prompt("Età:"));
 
-tokyoJujutsuHigh.push(
 
-    utente = {
-        name: nameUtente,
-        surname: surnameUtente,
-        age: ageUtente,
-        occupation: "Jujutsu Sorcer"
-    }
+if (genere == "maschio") {
+    tokyoJujutsuHigh.push(
 
-);
+        utente = {
+            name: nameUtente,
+            surname: surnameUtente,
+            age: ageUtente,
+            occupation: "Jujutsu Sorcer",
+            photo: "./img/Noritoshi_Kamo_29.webp"
+        }
+    
+    );
+} else if (genere == "femmina") {
+    tokyoJujutsuHigh.push(
+
+        utente = {
+            name: nameUtente,
+            surname: surnameUtente,
+            age: ageUtente,
+            occupation: "Jujutsu Sorcer",
+            photo: "./img/Kasumi_Miwa_29.webp"
+        }
+    
+    );
+};
+
 
 /* console.log(utente); */
 
@@ -111,6 +135,8 @@ console.log(
     -------------------------------------------------
     `
 );
+
+printCard (tokyoJujutsuHigh);
 
 
 
@@ -169,3 +195,35 @@ function printTokyoJujutsuHigh(tokyoJujutsuHigh) {
     };
 
 };
+
+// Bonus
+function printCard (tokyoJujutsuHigh) {
+
+    for (let i=0; i<tokyoJujutsuHigh.length; i++) {
+
+        document.getElementById("studentCard").innerHTML += 
+        `
+        <div class="card m-4" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4 pt-3">
+            <img src="${tokyoJujutsuHigh[i].photo}" class="img-fluid rounded-start" alt="${tokyoJujutsuHigh[i].surname} ${tokyoJujutsuHigh[i].name}">
+          </div>
+          <div class="col-md-8 text-center">
+            <div class="card-body">
+              <h3 class="card-title">${tokyoJujutsuHigh[i].surname} ${tokyoJujutsuHigh[i].name}</h3>
+              <h5 class="card-text">${tokyoJujutsuHigh[i].age}</h5>
+              <h5 class="card-text">${tokyoJujutsuHigh[i].occupation}</h5>
+              
+              
+              <img src="./img/code bar.png" class="img-fluid rounded-start" alt="Yuji Itadori">
+              
+
+            </div>
+          </div>
+        </div>
+        </div>
+        `
+
+    };
+
+}
